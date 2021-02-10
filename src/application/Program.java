ae57b9b1526d9946ac4856b27cdf4a84995d0055
kayquec13 <kayquecbenatti@gmail.com>
2021-02-10 19:17:47 -0300

    DaoFactorory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -15,6 +17,8 @@ public class Program {
 		
 		Seller seller = new Seller (21,"bob", "bob@gmail.com", new Date(), 3000.0, obj);
 
+		SellerDao sellerdao =  DaoFactory.createSellerDao();
+		
 		System.out.println(seller);
 
 
